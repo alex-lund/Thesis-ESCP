10ce52830abbdeba95e9a77f237bc152379003b7
alex-lund <allu-20@outlook.com>
2021-04-28 21:40:09 +0200

    v0.1

--- reddit&twitter-scraper.py
+++ reddit&twitter-scraper.py
@@ -1,37 +1,61 @@
+"""
+DATA SOURCING FILE (WEBSCRAPER)
+
+ALEXANDER LUND e201279 - THESIS ESCP
+"""
+
 import pandas as pd
 from textblob import TextBlob
 from prawcore.exceptions import Forbidden
 import requests
 from twitter_scraper import get_tweets
 import praw
+from praw.models import MoreComments, comment_forest
 import pysentiment2 as ps
 import demoji
 from collections import OrderedDict
 from collections import defaultdict
 
-reddit = praw.Reddit(client_id="_BcGe2c7efYCbw",
-                     client_secret="TMf68K8Qiztyos5EfFtT-GQ4cFnDLQ",
-                     redirect_uri="http://localhost:8080",
-                     password="B4ck2Geek1N3ss",
-                     user_agent="sustainable sentiments",
-                     username="sustainablesentiment",
+
+################# REDDIT #################
+
+
+reddit = praw.Reddit(client_id="***",
+                     client_secret="***",
+                     redirect_uri="***",
+                     password="***",
+                     user_agent="***",
+                     username="***",
                      )
 
-#reddit.read_only = False
-
+# verify that authentication works
 print(reddit.user.me())
 
+
+# key subreddits scraping
 targetsubreddits = ["investing", "finance", "wallstreetbets", "esg" ]
 
 hotposts = OrderedDict.fromkeys(targetsubreddits,[])
 
-d = defaultdict(list)
+dsubreddits = defaultdict(list)
 
 for keys in hotposts:
-    for submission in reddit.subreddit(keys).top(limit=30):
-        d[keys].append(submission.title)
+    for submission in reddit.subreddit(keys).top(limit=100):
+        dsubreddits[keys].append(submission.title)
 
-print(d)
+
+# key search words scraping
+targetsearchwords = ["esg", "sustainable investing", "responsible investment", "impact investing", "sustainable finance", "green finance", "corporate social responsibility", "esg investing", "socially responsible investing", "climate funds", "portfolio carbon footprint", "carbon finance", "positive screening", "best in class ESG", "sustainability index", "thematic investing", "triple bottom line"]
+
+searchposts = OrderedDict.fromkeys(targetsearchwords,[])
+
+dsearchwords = defaultdict(list)
+
+for keys in searchposts:
+    for sub in reddit.subreddit("all").search(keys):
+        dsearchwords[keys].append(sub.title)
+
+
 
 
 """textualanalysis = []

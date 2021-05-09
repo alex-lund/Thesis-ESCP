624ac4b0f3992e26c21a6a302df38cb7ceaca56a
alex-lund <allu-20@outlook.com>
2021-05-09 23:12:51 +0200

    v0.6

--- reddit&twitter-scraper.py
+++ reddit&twitter-scraper.py
@@ -7,17 +7,18 @@ ALEXANDER LUND e201279 - THESIS ESCP
 """
 
 import pandas as pd
-from textblob import TextBlob
-import nltk
-from prawcore.exceptions import Forbidden
 import requests
-from twitter_scraper import get_tweets
 import praw
 from praw.models import MoreComments, comment_forest
 import pysentiment2 as ps
 import demoji
 from collections import OrderedDict
 from collections import defaultdict
+from prawcore.exceptions import Forbidden
+from textblob import TextBlob
+import nltk
+from twitter_scraper import get_tweets
+import tweepy
 
 """nltk.download("brown")"""
 
@@ -38,28 +39,75 @@ print(reddit.user.me())
 
 
 # key subreddits scraping
-targetsubreddits = ["investing", "finance", "wallstreetbets", "esg"]
+targetsubreddits = ["greeninvestor", "sustainability", "climateoffensive", "greenfinance", "esginvesting"]
 
-hotposts = OrderedDict.fromkeys(targetsubreddits,[[]])
+hotposts = OrderedDict.fromkeys(targetsubreddits,[])
 
 dsubreddits = defaultdict(list)
 
 
 for keys in hotposts:
-    for submission in reddit.subreddit(keys).top(limit=100):
+    for submission in reddit.subreddit(keys).top(limit=60):
         dsubreddits[keys].append(submission.title)
         dsubreddits[keys].append(submission.created)
 
 
 subredditdf = pd.DataFrame().from_dict(dsubreddits)
 
-subredditdf = pd.DataFrame(subredditdf.values.reshape(-1, 8), columns = ["investing", "finance", "wallstreetbets", "esg", "investing-timestamp", "finance-timestamp", "wallstreetbets-timestamp", "esg-timestamp"])
+columns1 = []
 
-order1 = ["investing", "investing-timestamp", "finance", "finance-timestamp", "wallstreetbets", "wallstreetbets-timestamp", "esg", "esg-timestamp"]
+for i in targetsubreddits:
+    columns1.append(i)
+    if columns1 == targetsubreddits:
+        for i in targetsubreddits:
+            columns1.append(f"{i}-timestamp")
 
-subredditdf = subredditdf.reindex(columns= order1)
 
-"""
+subredditdf = pd.DataFrame(subredditdf.values.reshape(-1, len(columns1)), columns = [columns1])
+
+order1 = []
+
+for i in targetsubreddits:
+    order1.append(i)
+    order1.append(i + "-timestamp")
+
+
+subredditdf = subredditdf.reindex(columns= [order1])
+
+body1 = pd.Series(subredditdf[subredditdf.columns[::2]].values.ravel())
+
+timestamp1 = pd.Series(subredditdf[subredditdf.columns[1::2]].values.ravel())
+
+subreddf = pd.concat([body1.rename("body"), timestamp1.rename("timestamp")], axis = 1).dropna()
+
+#top level comment scraping
+
+subcomments = OrderedDict.fromkeys(targetsubreddits,[])
+
+dsubcomments = defaultdict(list)
+
+for keys in subcomments:
+    for submission in reddit.subreddit(keys).top("all"):
+        for top_level_comment in submission.comments[1:]:
+            if isinstance(top_level_comment, MoreComments):
+                continue
+            dsubcomments[keys].append(top_level_comment.body)
+            dsubcomments[keys].append(top_level_comment.created_utc)
+
+subcommentsdf = pd.DataFrame().from_dict(dsubcomments, orient = "index")
+
+subcommentsdf = subcommentsdf.transpose()
+
+subcommentsdf = pd.DataFrame(subcommentsdf.values.reshape(-1, len(columns1)), columns = [columns1])
+
+subcommentsdf = subcommentsdf.reindex(columns= [order1])
+
+body2 = pd.Series(subcommentsdf[subcommentsdf.columns[::2]].values.ravel())
+
+timestamp2 = pd.Series(subcommentsdf[subcommentsdf.columns[1::2]].values.ravel())
+
+subcomdf = pd.concat([body2.rename("body"), timestamp2.rename("timestamp")], axis = 1).dropna()
+
 # key search words scraping
 targetsearchwords = ["esg", "sustainable investing", "responsible investment", "impact investing", "sustainable finance", "green finance", "corporate social responsibility", "esg investing", "socially responsible investing", "climate funds", "portfolio carbon footprint", "carbon finance", "positive screening", "best in class ESG", "sustainability index", "thematic investing", "triple bottom line"]
 
@@ -68,21 +116,41 @@ searchposts = OrderedDict.fromkeys(targetsearchwords,[])
 dsearchwords = defaultdict(list)
 
 for keys in searchposts:
-    for sub in reddit.subreddit("all").search(keys):
+    for sub in reddit.subreddit("all").search(keys, limit= 50):
         dsearchwords[keys].append(sub.title)
         dsearchwords[keys].append(sub.created)
 
+searchwordsdf = pd.DataFrame().from_dict(dsearchwords, orient="index")
 
-searchcomments = OrderedDict.fromkeys(targetsearchwords,[])
+columns2 = []
 
-dsearchcomments = defaultdict(list)
+for i in targetsearchwords:
+    columns2.append(i)
+    if columns2 == targetsearchwords:
+        for i in targetsearchwords:
+            columns2.append(f"{i}-timestamp")
 
-for keys in searchposts:
-    for sub in reddit.subreddit("all").search(keys):
-        for comments in reddit.subreddit("all").comments():
-            dsearchcomments[keys].append(sub.comments)
-            dsearchcomments[keys].append(sub.created)
-"""
+
+searchwordsdf = pd.DataFrame(searchwordsdf.values.reshape(-1, len(columns2)), columns = [columns2])
+
+order2 = []
+
+for i in targetsearchwords:
+    order2.append(i)
+    order2.append(i + "-timestamp")
+
+searchwordsdf = searchwordsdf.reindex(columns= [order2])
+
+body3 = pd.Series(searchwordsdf[searchwordsdf.columns[::2]].values.ravel())
+
+timestamp3 = pd.Series(searchwordsdf[searchwordsdf.columns[1::2]].values.ravel())
+
+searchwddf = pd.concat([body3.rename("body"), timestamp3.rename("timestamp")], axis = 1).dropna()
+
+print("Scraping Done!")
+
+
+
 
 """textualanalysis = []
 
